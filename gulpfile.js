488561205bce9b9775b1ee68f57b6391ee59a454
William Bai <williambai2001@gmail.com>
2016-08-16 16:18:37 +0800

    update gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
 
+/**
+ * 根据backbone框架的要求，将模板文件(*.tpl)内嵌在布局文件(.html)中
+ * @param  {[type]} ){	var target        [description]
+ * @return {[type]}         [description]
+ */
 gulp.task('backbone', function(){
 	var target = gulp.dest('./public');
 	var layout = gulp.src('./public/tpl/backbone/backbone.html');
